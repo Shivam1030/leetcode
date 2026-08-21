adc675a4115951307967afc96cf7e0fb6eda1a8b
Shivam <164848074+Shivam1030@users.noreply.github.com>
2026-08-21 18:03:42 +0530

    Time: 27 ms (59.53%), Space: 19.2 MB (84.82%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.py
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -1,18 +1,7 @@
 class Solution:
-    def sum_of_k_consecutive_multiples_of_x(self, x, k):
-        return x * k * (k+1) // 2
-
     def sumOfMultiples(self, n: int) -> int:
-        no_div_by_3 = n // 3
-        no_div_by_5 = n // 5
-        no_div_by_7 = n // 7
-
-        no_div_by_15 = n // 15
-        no_div_by_35 = n // 35
-        no_div_by_21 = n // 21
-
-        no_div_by_105 = n // 105
-
-        sum = self.sum_of_k_consecutive_multiples_of_x(3, no_div_by_3) + self.sum_of_k_consecutive_multiples_of_x(5, no_div_by_5) + self.sum_of_k_consecutive_multiples_of_x(7, no_div_by_7) - self.sum_of_k_consecutive_multiples_of_x(15, no_div_by_15) - self.sum_of_k_consecutive_multiples_of_x(35, no_div_by_35) - self.sum_of_k_consecutive_multiples_of_x(21, no_div_by_21) + self.sum_of_k_consecutive_multiples_of_x(105, no_div_by_105)
-
-        return int(sum)
+        count = 0
+        for i in range(1, n+1):
+            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+                count += i
+        return count
